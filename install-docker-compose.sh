ef856604ee74f91e063ba88012a08100a7f38a03
vsushche <Vadim.Sushchenko@genesys.com>
2021-10-03 09:36:12 +0300

    add nodejs installation

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -4,3 +4,4 @@ echo Install Docker-Compose
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+s
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
-curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-tools.sh | sh
-curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-git.sh | sh
+curl https://raw.githubusercontent.com/vsushche/station-prepare/main/install-tools.sh | sh
+curl https://raw.githubusercontent.com/vsushche/station-prepare/main/install-git.sh | sh
+curl https://raw.githubusercontent.com/vsushche/station-prepare/main/install-nodejs.sh | sh
 
