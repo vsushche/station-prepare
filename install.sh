282fd7ecceee57869d3578b289d14986d40c1566
vsushche <Vadim.Sushchenko@genesys.com>
2021-10-03 09:42:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-curl https://raw.githubusercontent.com/vsushche/station-prepare/main/install-tools.sh | sh
-curl https://raw.githubusercontent.com/vsushche/station-prepare/main/install-git.sh | sh
-curl https://raw.githubusercontent.com/vsushche/station-prepare/main/install-nodejs.sh | sh
+curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-tools.sh | sh
+curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-git.sh | sh
+curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-nodejs.sh | sh
+curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-docker.sh | sh
 
