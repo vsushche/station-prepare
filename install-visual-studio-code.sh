8e03664d9910739ae07377619bf96384f5a07d0e
vsushche <Vadim.Sushchenko@genesys.com>
2021-10-03 12:18:29 +0300

    add installation of Visual Studio Code

--- install-visual-studio-code.sh
+++ install-visual-studio-code.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 echo Install Visual Studio Code
-yum -y remove code || true
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-yum -y check-update || true
-yum -y install code
+sudo yum -y remove code || true
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo yum -y check-update || true
+sudo yum -y install code
 mkdir ~/.vscode
-cat << EOF > /usr/share/applications/visual-studio-code.desktop
+sudo cat << EOF > /usr/share/applications/visual-studio-code.desktop
 [Desktop Entry]
 Version=1.0
 Name=Visual Studio Code
--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/insta
 curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-nodejs.sh | sh
 curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-docker.sh | sh
 curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-docker-compose.sh | sh
+curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-visual-studio-code.sh | sh
 
