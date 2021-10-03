f2a6734a8356435e30ee879d07c93cb956ca07bc
vsushche <Vadim.Sushchenko@genesys.com>
2021-10-03 09:25:35 +0300

    add install.sh content

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-tools.sh | sh
+curl -fsSL https://raw.githubusercontent.com/vsushche/station-prepare/main/install-git.sh | sh
+
